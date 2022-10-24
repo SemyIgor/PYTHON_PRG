7dabcb9978a8499bfc278e6f10ab0c5576a423b0
HenkSeed <adaner@inbox.ru>
2022-10-24 23:29:34 +0700

    Added Seminar08 HomeWork

---
+++ Seminar08_HW/logger.py
@@ -0,0 +1,64 @@
+﻿import csv
+import json
+import user_interface as ui
+import controller as con
+
+
+# Read data from the files module
+def get_txt_data():
+    with open(ui.get_file_name()+'.txt', 'r', encoding='utf-8-sig') as file:
+        txt_book = []
+        # fields_number = file.readline().rstrip()
+        while True:  # Считываем по одному абоненту
+            txt_book_script =\
+                (file.readline().rstrip(),  # Убираем символ перевода строки
+                    file.readline().rstrip(),
+                    file.readline().rstrip(),
+                    file.readline().rstrip())
+            if txt_book_script[0] == '':
+                break
+            txt_book.append(txt_book_script)
+    return txt_book
+
+
+def get_csv_data():
+    with open(ui.get_file_name()+'.csv', encoding='utf-8-sig') as c_file:
+        txt_book = []
+        while True:  # Считываем по одному абоненту
+            txt_book_script = tuple(c_file.readline().rstrip().split(';'))
+            if txt_book_script[0] == '':
+                break
+            txt_book.append(txt_book_script)
+    return txt_book
+
+
+def get_json_data():
+    with open(ui.get_file_name()+'.json', 'r', encoding='utf-8-sig') as file:
+        txt_book = json.load(file)
+    return txt_book
+
+# Save data to the files module
+
+
+def save_txt_data(active_base):
+    with open(ui.get_file_name()+'.txt', 'w') as file:
+        for i in active_base:
+            file.write(
+                f'{i[0]}\n{i[1]}\n{i[2]}\n{i[3]}\n')
+            # f'{i[0]}; {i[1]}; {i[2]}; {i[3]}\n')
+    return
+
+
+def save_csv_data(active_base):
+    with open(ui.get_file_name()+'.csv', 'w') as file:
+        for i in active_base:
+            file.write(
+                f'{i[0]}; {i[1]}; {i[2]}; {i[3]}\n')
+            # f'{i[0]}\n{i[1]}\n{i[2]}\n{i[3]}\n')
+    return
+
+
+def save_json_data(active_base):
+    with open(ui.get_file_name()+'.json', 'w', encoding='utf-8-sig') as file:
+        json.dump(active_base, file, ensure_ascii=False)
+    return
---
+++ Seminar08_HW/user_interface.py
@@ -0,0 +1,60 @@
+﻿global active_base
+
+
+def choice_base_convertor():
+    print('1 - Чтение / Запись файла\n'
+          '2 - Работа с базой\n'
+          '3 - Просмотр загруженной базы\n'
+          '0 - Выход из программы')
+    return input()
+
+
+def convertor_menu():
+    print('1 - Чтение txt файла\n'
+          '2 - Запись txt файла\n'
+          '3 - Чтение csv файла\n'
+          '4 - Запись csv файла\n'
+          '5 - Чтение json файла\n'
+          '6 - Запись json файла')
+    return input()
+
+
+def base_option_menu():
+    print('1 - Просмотр базы\n'
+          '2 - Добавить запись\n'
+          '3 - Удалить запись\n'
+          '4 - Редактировать поля записи\n'
+          '5 - Редактировать структуру базы\n'
+          '6 - Сохранить изменения в файл')
+    return input()
+
+
+def base_add_menu():
+    new_id = input('Введите новый ID: ')
+    new_surname = input('Введите новую фамилию: ')
+    new_name = input('Введите новое имя: ')
+    new_patronymic = input('Введите новое отчество: ')
+    new_phone = input('Введите новый телефон: ')
+    return (new_id, new_surname, new_name, new_patronymic, new_phone)
+
+
+def base_del_menu():
+    del_id = input('Введите ID записи к удалению: ')
+    return (del_id)
+
+
+def base_person_edit_menu():
+    edit_id = input('Введите ID записи к редактированию: ')
+    return (edit_id)
+
+
+def get_file_name():
+    file_name = input('Введите имя файла без расширения:\n')
+    return f'Seminar08_HW\{file_name}'
+
+
+def print_base(active_base):
+    for el in active_base:
+        print(el)
+    print('\n')
+    return
